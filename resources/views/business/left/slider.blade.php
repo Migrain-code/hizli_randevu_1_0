ab12f3d48869c34a4fb42eef495ce4207eb18fa8
migrain.code <migrain.code@gmail.com>
2024-04-23 18:34:31 +0300

    -- işletme component güncellendi

--- resources/views/business/left/slider.blade.php
+++ resources/views/business/left/slider.blade.php
@@ -1,18 +1,18 @@
 <div class="saloonDetailSliders mb-4 mobile-hidden">
     <div id="" class="owl-carousel owl-theme saloonDetailSlider1">
-        @foreach($business->sliders as $bSlider)
+        @foreach($business->gallery as $bSlider)
             <div class="item">
                 <div class="saloonDetailSliderBigPhoto">
-                    <img src="{{image($bSlider->image)}}" alt="{{$bSlider->name}}"/>
+                    <img src="{{image($bSlider->way)}}" alt="{{$bSlider->name}}"/>
                 </div>
             </div>
         @endforeach
     </div>
     <div id="" class="owl-carousel owl-theme saloonDetailSlider2">
-        @foreach($business->sliders as $bSlider)
+        @foreach($business->gallery as $bSlider)
             <div class="item">
                 <div class="saloonDetailSliderBigPhoto">
-                    <img src="{{image($bSlider->image)}}" alt="{{$bSlider->name}}"/>
+                    <img src="{{image($bSlider->way)}}" alt="{{$bSlider->name}}"/>
                 </div>
             </div>
         @endforeach
