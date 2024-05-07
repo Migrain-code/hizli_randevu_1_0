fc1de08c9b9e8f32a5d49f168bd273eea4e8492c
migrain.code <migrain.code@gmail.com>
2024-05-07 19:14:32 +0300

    -- tom-select.complete.min.js güncellendi

--- resources/views/customer/profile/setting.blade.php
+++ resources/views/customer/profile/setting.blade.php
@@ -3,7 +3,19 @@
 @section('meta_description', "Kullanıcı Profilim")
 @section('styles')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
+    <style>
+        .select2-container--default .select2-selection--single {
+            background-color: #fff;
+            border: 1px solid #aaa;
+            border-radius: 4px;
+            margin-top: 6px;
+            height: 57px;
+            padding-left: 15px;
+            padding-top: 5px;
+            border-radius: 15px;
+            border: 1px solid rgba(67, 80, 110, 0.2) !important;
+        }
+    </style>
 @endsection
 @section('content')
 
