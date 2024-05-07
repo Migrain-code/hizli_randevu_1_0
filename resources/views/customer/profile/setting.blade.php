a883aa7ca4ffc20c4b33ae95e96edc14347acebd
migrain.code <migrain.code@gmail.com>
2024-05-07 17:14:36 +0300

    -- setting

--- resources/views/customer/profile/setting.blade.php
+++ resources/views/customer/profile/setting.blade.php
@@ -111,7 +111,7 @@
                                         <div class="col-lg-6">
                                             <div class="mb-3">
                                                 <div class="row">
-                                                    <select class="form-select" name="city_id" id="city_select">
+                                                    <select class="" name="city_id" id="city_select">
                                                         <option selected>İl</option>
                                                         @foreach($allCities as $city)
                                                             <option value="{{$city->id}}" @selected(auth('customer')->user()->city_id==$city->id)>{{$city->name}}</option>
