cb7c220d235d328d1d2988daca67f4abdf84d43f
migrain.code <migrain.code@gmail.com>
2024-05-07 15:26:07 +0300

    -- blog detay düzenlendi

--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -47,7 +47,7 @@
                                                 <div class="row">
                                                     @forelse($heads as $head)
                                                         <div class="col-lg-4">
-                                                            <a href="#head-{{$loop->index}}">{{$head}} </a>
+                                                            <a href="#head-{{$loop->index}}">{!! $head !!} </a>
                                                         </div>
                                                     @empty
                                                     @endforelse
