a6fbcce51b52b552eca15778e08e23948951bf6c
migrain.code <migrain.code@gmail.com>
2024-05-26 18:18:33 +0300

    -- randevu izin ayarı çözüldü

--- resources/views/appointment/step1.blade.php
+++ resources/views/appointment/step1.blade.php
@@ -437,11 +437,9 @@
                                 clickedDate(nextDay);
 
                                 let newClickedTime = formatDate(nextDay);
-                                if (newClickedTime instanceof Date && !isNaN(newClickedTime)) {
-                                    var targetInput = document.querySelector('input[data-clock="' + newClickedTime + '"]');
-                                    targetInput.checked = true;
-                                }
 
+                                var targetInput = document.querySelector('input[data-clock="' + newClickedTime + '"]');
+                                targetInput.checked = true;
                             } else if (result.isDismissed && result.dismiss === Swal.DismissReason.cancel) {
                                 window.location.href = "{{route('business.detail', $business->slug)}}"
                             }
