1ad76d26fee580dfef49f672f10f739c51c0cd19
migrain.code <migrain.code@gmail.com>
2024-05-27 19:31:15 +0300

    -- il ilçe güncelleme hatası çözdüldü

--- app/Http/Controllers/Customer/ProfileController.php
+++ app/Http/Controllers/Customer/ProfileController.php
@@ -14,7 +14,7 @@ class ProfileController extends Controller
 {
     public function update(Request $request)
     {
-        $request->dd();
+
         $request->validate([
             'phone' => "required",
             'name' => "required",
