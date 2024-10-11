cdd0160a569c65cde67dbc6bcba93333949c23cd
migrain.code <migrain.code@gmail.com>
2024-10-11 21:56:19 +0300

    -- personel takvim kontrol

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -103,12 +103,22 @@ class HomeController extends Controller
             $unisexServiceCategories = $unisexServicesArray->groupBy('categorys.name');
             $unisexServices = $this->transformServices($unisexServiceCategories);
             if ($business->slug == "test-salon-3"){
+
+// Tatil günleri sayısı 7'den fazla olan personelin 7 günden sonraki kayıtlarını silme
                 $personels = [];
-                foreach(Personel::all() as $personel){
-                    if ($personel->restDayAll()->count() > 7){
+                foreach (Personel::all() as $personel) {
+                    $restDays = $personel->restDayAll();
+                    if ($restDays->count() > 7) {
+                        // 7'den fazla tatil günü var, ilk 7 günü hariç geri kalanları al ve sil
+                        $toDelete = $restDays->sortBy('date')->skip(7);
+                        foreach ($toDelete as $restDay) {
+                            $restDay->delete();
+                        }
                         $personels[] = $personel->id;
                     }
                 }
+
+// İşlemi kontrol etmek için personel id'lerini dök
                 dd($personels);
             }
             return view('business.detail', compact('business', 'dayList', 'womanServices', 'manServices', 'unisexServices'));
