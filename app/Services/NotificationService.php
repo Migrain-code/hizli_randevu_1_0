4505d1e8a266114eb7472545f3213a8ffba7830d
migrain.code <migrain.code@gmail.com>
2024-11-16 03:29:33 +0300

    -- bildirim servisi güncellendi

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -2,21 +2,31 @@
 
 namespace App\Services;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class NotificationService
 {
-    public static function sendPushNotification($expoToken, $title, $body)
+    public static function sendPushNotification($expoToken, $title, $body, $link = null)
     {
-        $client = new Client();
-        $response = $client->post(env('EXPO_PUSH_URL'), [
-            'json' => [
-                'to' => $expoToken,
-                'title' => $title,
-                'body' => $body,
-                'sound'=> 'default',
-            ],
-        ]);
+        try {
+            $client = new Client();
+            $response = $client->post(env('EXPO_PUSH_URL'), [
+                'json' => [
+                    'to' => $expoToken,
+                    'title' => $title,
+                    'body' => $body,
+                    'data' => [
+                        "link" => isset($link) ? $link : null,
+                    ],
+                    'sound'=> 'default',
+                ],
+            ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+            return json_decode($response->getBody()->getContents(), true);
+        }
+        catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+
     }
 }
